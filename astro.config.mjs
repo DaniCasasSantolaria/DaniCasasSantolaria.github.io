b22f2e8ed1f2d8b0855211a68070fe3576f480cc
Daniel Casas <daniel.casas@estudiant.enti.cat>
2026-05-11 20:58:32 +0200

    add images and other info

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,9 @@ import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   site: 'https://danicasassantolaria.github.io',
+  devToolbar: {
+    enabled: false,
+  },
   vite: {
     plugins: [tailwindcss()],
   },
